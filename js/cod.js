8da925e45643d1b340ec4ec0dfe414682374bc15
MatheusArtilles <mateusaaa2424@gmail.com>
2023-03-12 16:01:51 -0300

    subindo js

--- js/cod.js
+++ js/cod.js
@@ -1,1 +1,38 @@
+const form = document.querySelector("form");
+const input = document.querySelectorAll(".campo");
+let cardsArea = document.querySelector(".cards-area");
+
+//titulo, personagem, img, descricao
+function criaCard(campos) {
+    let div = document.createElement("div");
+    div.classList.add("card");
+
+    let imagem = document.createElement("img");
+    imagem.src = `${campos[2].value}`;
+
+    let h3 = document.createElement("h3");
+    h3.classList.add("personagem");
+    h3.innerHTML = `${campos[0].value}`;
+
+    let h4 = document.createElement("h4");
+    h4.classList.add("card-titulo");
+    h4.innerHTML = `${campos[1].value}`;
+
+    let p = document.createElement("p");
+    p.classList.add("descricao");
+    p.innerHTML = `${campos[3].value}`;
+
+    div.appendChild(imagem);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(p);
+
+    cardsArea.appendChild(div);
+    return div;
+}
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+})
+
 
